6d7b0b617e405faa08c05e48ed8bcd832ea35d02
Louis Raymond <louis.raymond@alumni.york.ac.uk>
2018-07-16 09:41:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers= function(array){
+  return array.slice(0,2)
+};
+
+const returnLastTwoDrivers =function (array){
+  return array.slice(-2)
+};
+
+const selectingDrivers= [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+const createFareMultiplier = function (int){
+    return function multiply(price){
+      return price*int
+    }
+  }
+
+
+
+const fareDoubler = createFareMultiplier(2);
+
+
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function(drivers, whichDrivers){
+  return whichDrivers(drivers);
+  }
+
