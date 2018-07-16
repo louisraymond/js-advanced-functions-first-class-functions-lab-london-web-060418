1d0f2dabb08848b9174ee4fdb18c47d63525aee6
Louis Raymond <louis.raymond@alumni.york.ac.uk>
2018-07-16 09:24:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // Code your solution in this file!
 const returnFirstTwoDrivers= function(array){
   return array.slice(0,2)
-}
+};
 
-const returnLastTwoDrivers =function (array)
+const returnLastTwoDrivers =function (array){
   return array.slice(-2)
+}
